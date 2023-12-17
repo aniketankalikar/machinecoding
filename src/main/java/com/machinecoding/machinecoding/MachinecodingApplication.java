bf0996adc8a82034d31e92c8079fe644a48ac98b
Aniket <aniket@clayfin.com>
2023-12-18 00:13:59 +0530

    Bookmyshow

---
+++ src/main/java/com/machinecoding/machinecoding/MachinecodingApplication.java
@@ -0,0 +1,13 @@
+package com.machinecoding.machinecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachinecodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachinecodingApplication.class, args);
+	}
+
+}
